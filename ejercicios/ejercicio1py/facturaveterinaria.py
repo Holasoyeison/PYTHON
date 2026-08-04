c97cf1521aa177eeaea4cccc2dfcf5c6fe04013e
Yeison Serrano <you@example.com>
2026-08-03 19:31:42 -0500

    Nuevos ejercicios Python

--- ejercicios/ejercicio1py/facturaveterinaria.py
+++ ejercicios/ejercicio1py/facturaveterinaria.py
@@ -7,12 +7,15 @@ tipoMascota= input("Tipo de nascota: ")
 valorConsulta= float(input("Precio de la consulta: $"))
 medicamentosValor= float(input("Precio de los medicamentos: $"))
 OtroServicio= float(input("Otros servicios ofrecidos: $"))
+iva=0.19
+totalPago= (valorConsulta+medicamentosValor+OtroServicio)*iva
+totalPago= totalPago+iva
 
-totalPago= valorConsulta+medicamentosValor+OtroServicio
+print ("=======FACTURA=======")
 
 print("==============================")
 print("El nombre del propietario de la mascota: ",nombrePropietario)
 print("Nombre d ela mascota:", nombreMascota)
 print("Mascota tipo:", tipoMascota)
-
+print("Iva: ", iva,)
 print ("=====TOTAL A PAGAR ES:====", totalPago, "Pesos.")
---
+++ ejercicios/ejercicio1py/practica7.py
@@ -0,0 +1,10 @@
+print("=====VALIDACIÓN CONTRASEÑA=====")
+
+contrasena ="admin123"
+
+ingreso = input("Por favor, ingrese la contraseña.")
+
+if ingreso == contrasena:
+    print("contraseña válida. Bienvenido.")
+else: 
+    print("Contraseña invalida. vuelve a intentarlo ")    
