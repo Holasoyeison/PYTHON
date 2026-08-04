3e9b869c28be0c294e7fb7178d52c55a9b7b6dee
Yeison Serrano <you@example.com>
2026-08-04 08:33:21 -0500

    first commit

---
+++ ejercicios/ejercicio1py/Practica8.py
@@ -0,0 +1,13 @@
+print("====LA SUMA TOTAL DE LOS NÚMEROS====")
+
+suma = 0
+numero = 1
+
+while numero <=50:
+    suma = suma + numero
+    numero= numero +1
+
+print("La suma de los números del 1 al 50 es: ", suma)
+
+
+
