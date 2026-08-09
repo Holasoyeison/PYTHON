1bf42cec9911a6448ba23a2b35af3355a55bc1fe
Yeison Serrano <you@example.com>
2026-08-08 21:10:36 -0500

    Primer commit

---
+++ ejercicios/ejercicio1py/condicionales3.py
@@ -0,0 +1,11 @@
+# programa que pida al usuario dos números y devuelva su división. Si el usuario no introduce números debe devolver un aviso de error y si el divisor es cero también.
+print("======DIVISION=======")
+
+dividendo=float(input("Ingrese el dividendo: "))
+divisor=float(input("Ingresa el número divisor: "))
+
+if divisor==0:
+    print("Error! No se puede dividir entre cero.")
+else: 
+    resultado =dividendo/divisor
+    print("El resultado d ela division es: ",resultado)
