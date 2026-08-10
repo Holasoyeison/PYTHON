8f21a179f2cab325ca700294f2f2ca81b55c406b
Yeison Serrano <you@example.com>
2026-08-09 21:27:51 -0500

    Agregue un ciclo al codigo

--- ejercicios/ejercicio1py/condicionales11.py
+++ ejercicios/ejercicio1py/condicionales11.py
@@ -2,6 +2,9 @@
 print("======CLASIFICACION DE NOTAS======")
 
 nota =float(input("Ingresa la nota del estudiante: "))
+while nota <0 or nota >5: 
+    print ("Nota no válida. Debe estar entre 0 y 5.")
+    nota= float(input("Ingrese la nota del estudiante:"))
 
 if nota <=2.9:
     print ("Reprobado.")
