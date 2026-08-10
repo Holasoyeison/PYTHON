d1244869472466a61ee0e4c94d3b121d0f28e36b
Yeison Serrano <you@example.com>
2026-08-09 21:06:22 -0500

    Primer commit

---
+++ ejercicios/ejercicio1py/condicionales10.py
@@ -0,0 +1,18 @@
+#Escribir un programa que pregunte al usuario el valor de una compra y calcule el descuento:
+print("=====DESCUENTO TIENDA=====")
+
+valorCompra=float(input("Ingrese el valor de la compra: "))
+
+if valorCompra<50:
+    descuento="No tiene descuento."
+    valorTotal=valorCompra
+elif valorCompra<=100:
+    descuento = valorCompra*0.10
+    valorTotal= valorCompra-descuento
+else:
+    descuento=valorCompra*0.20
+    valorTotal=valorCompra-descuento
+print ("="*32)
+print ("El valor de tu compra es: ", valorCompra, "Euros")
+print ("El descuento de tu compra es: ", descuento, "Euros")
+print ("El valor a pagar es: ",valorTotal, "Euros")
